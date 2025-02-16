d188189666219b5658eaf3c1f1d7f11ef21a5396
Daniel88dev <64728456+Daniel88dev@users.noreply.github.com>
2025-02-16 16:03:41 +0100

    finish today 16-2-2025

--- app/(auth)/home/page.tsx
+++ app/(auth)/home/page.tsx
@@ -1,8 +1,12 @@
-const HomePage = () => {
+import { currentUser } from "@clerk/nextjs/server";
+
+const HomePage = async () => {
+  const user = await currentUser();
+
   return (
     <div className="w-full max-w-5xl items-center justify-between font-mono text-sm">
-      <h1 className="w-full max-w-5xl items-center justify-between font-mono text-sm">
-        Welcome to TaskMaster
+      <h1 className="mb-4 text-4xl font-extrabold">
+        Welcome to TaskMaster {user?.firstName ?? ""}
       </h1>
       <p>Task overview</p>
     </div>
