e44715b125deaa7d2388244825860e3ca2e01ce3
Daniel88dev <64728456+Daniel88dev@users.noreply.github.com>
2025-04-16 19:19:04 +0200

    Removed default picture for user

--- app/(auth)/_components/defaultUserIcon.tsx
+++ app/(auth)/_components/defaultUserIcon.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 const DefaultUserIcon = () => {
   return (
     <Avatar className="w-7 h-7 mt-1">
-      <AvatarImage src={"/user.png"} />
+      <AvatarImage />
       <AvatarFallback />
     </Avatar>
   );
