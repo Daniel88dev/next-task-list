41139ad2b9eed9a46d14e7f1821cbcbecab1d0be
Daniel88dev <64728456+Daniel88dev@users.noreply.github.com>
2025-04-18 17:43:52 +0200

    Added better pagination component

--- components/ShoppingTableComponents/ShoppingDataTable.tsx
+++ components/ShoppingTableComponents/ShoppingDataTable.tsx
@@ -23,6 +23,7 @@ import {
 import { DataTableFacetedFilter } from "@/components/TaskTableComponents/DataTableFacetedFilter";
 import { categoryList } from "@/app/(auth)/shopping/_components/categoryTypes";
 import { Button } from "@/components/ui/button";
+import { DataTablePagination } from "@/components/ui/DataTablePagination";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -146,28 +147,7 @@ export function ShoppingDataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of{" "}
-        {table.getFilteredRowModel().rows.length} row(s) selected.
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-      </div>
+      <DataTablePagination table={table} />
     </div>
   );
 }
